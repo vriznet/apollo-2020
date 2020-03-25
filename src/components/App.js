8f022aa4758e68997b988e75768e761d3a1ac79e
vriznet <vriz@vriz.net>
2020-03-25 14:28:13 +0900

    0.3 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../styles/GlobalStyles';
+import Theme from '../styles/Theme';
+import Home from '../routes/Home';
+import Detail from '../routes/Detail';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/:id" component={Detail} />
+      </Router>
+    </ThemeProvider>
+  );
+};
 
 export default App;
