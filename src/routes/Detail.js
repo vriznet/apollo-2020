2231d7a83afa16ad7bac04f8e681bb6e82c4795c
vriznet <vriz@vriz.net>
2020-03-26 13:36:16 +0900

    1.2 GET_MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,30 @@
-export default () => 'Detail';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  let { id } = useParams();
+  id = parseInt(id);
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id },
+  });
+  if (loading) {
+    return 'loading';
+  }
+  if (!loading && data && data.movie) {
+    return data.movie.title;
+  }
+  return 'Detail';
+};
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
+import Movie from '../components/Movie';
+
+const Container = styled.div`
+  width: 100%;
+  background-color: gray;
+  height: 500px;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  color: white;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 const GET_MOVIES = gql`
   {
@@ -13,10 +41,16 @@ const GET_MOVIES = gql`
 
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES);
-  if (loading) {
-    return 'loading...';
-  }
-  if (data && data.movies) {
-    return data.movies.map(m => <h1>{m.id}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+    </Container>
+  );
 };
